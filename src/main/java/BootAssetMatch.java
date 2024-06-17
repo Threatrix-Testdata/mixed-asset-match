ca9af4cc5d72fd2f6e7dfc9330d923c964d1a9f6
Roman  Sluchevskii <roman.sluchevsky@gmail.com>
2024-06-17 18:20:55 +0300

    Update BootAssetMatch.java

--- src/main/java/BootAssetMatch.java
+++ src/main/java/BootAssetMatch.java
@@ -421,21 +421,73 @@ public class SpringApplicationRunner {
 
 	}
 
-	/**
-	 * Thread to watch for file changes and trigger recompile/reload.
-	 */
-	private class FileWatchThread extends Thread {
+	private SourceOptions(List<?> nonOptionArguments, ClassLoader classLoader) {
+		List<String> sources = new ArrayList<>();
+		int sourceArgCount = 0;
+		for (Object option : nonOptionArguments) {
+			if (option instanceof String) {
+				String filename = (String) option;
+				if ("--".equals(filename)) {
+					break;
+				}
+				List<String> urls = new ArrayList<>();
+				File fileCandidate = new File(filename);
+				if (fileCandidate.isFile()) {
+					urls.add(fileCandidate.getAbsoluteFile().toURI().toString());
+				}
+				else if (!isAbsoluteWindowsFile(fileCandidate)) {
+					urls.addAll(ResourceUtils.getUrls(filename, classLoader));
+				}
+				for (String url : urls) {
+					if (isSource(url)) {
+						sources.add(url);
+					}
+				}
+				if (isSource(filename)) {
+					if (urls.isEmpty()) {
+						throw new IllegalArgumentException("Can't find " + filename);
+					}
+					else {
+						sourceArgCount++;
+					}
+				}
+			}
+		}
+		this.args = Collections.unmodifiableList(nonOptionArguments.subList(sourceArgCount, nonOptionArguments.size()));
+		Assert.isTrue(!sources.isEmpty(), "Please specify at least one file");
+		this.sources = Collections.unmodifiableList(sources);
+	}
 
-		private long previous;
+	private boolean isAbsoluteWindowsFile(File file) {
+		return isWindows() && file.isAbsolute();
+	}
 
-		private List<File> sources;
+	private boolean isWindows() {
+		return File.separatorChar == '\\';
+	}
 
-		FileWatchThread() {
-			super("filewatcher-" + (watcherCounter++));
-			this.previous = 0;
-			this.sources = getSourceFiles();
-			for (File file : this.sources) {
-				if (file.exists()) {
-					long current = file.lastModifi
+	private boolean isSource(String name) {
+		return name.endsWith(".java") || name.endsWith(".groovy");
+	}
+
+	public List<?> getArgs() {
+		return this.args;
+	}
+
+	public String[] getArgsArray() {
+		return this.args.stream().map(this::asString).toArray(String[]::new);
+	}
+
+	private String asString(Object arg) {
+		return (arg != null) ? String.valueOf(arg) : null;
+	}
+
+	public List<String> getSources() {
+		return this.sources;
+	}
+
+	public String[] getSourcesArray() {
+		return StringUtils.toStringArray(this.sources);
+	}
 
 }
